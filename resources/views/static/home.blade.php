af25e6fca3362609a1117e176d0b71085c7b384e
Nyan Lynn Htut <lynnhtut87@gmail.com>
2015-06-29 19:32:43 +0630

    Update text at homepage

--- resources/views/static/home.blade.php
+++ resources/views/static/home.blade.php
@@ -6,12 +6,8 @@
 	<div class="hero center white">
 		<div class="app-container">
 			<h2>2015 Myanmar Election API</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<p>Welcome to MyanmarAPI, a civic information API for the 2015 elections and beyond!</p>
+			<p>Developers are using MyanmarAPI to build mobile and web applications.</p>
 
 			@if (Auth::guest())
 
@@ -54,12 +50,12 @@
 			<div class="card hoverable">
         		<div class="card-content grey-text text-darken-2">
           			<span class="card-title grey-text text-darken-2">Contribute</span>
-					<p>If you want to develop an app using the API, 
-					you need to register for your developer account.</p>
-					<p>See detail guide for registration at documentation link.</p>
+					<p>MyanmarAPI is a completely open-source project. 
+					You can find all the code for the API at our <a href="https://github.com/MyanmarAPI" target="_blank">GitHub organization page</a>.</p>
+					<p>See detail guide for contributing at documentation link.</p>
         		</div>
         		<div class="card-action">
-          			<a href="#" class="cyan-text text-darken-2">How to Register</a>
+          			<a href="#" class="cyan-text text-darken-2">How to Contribute</a>
         		</div>
       		</div> <!-- end of div.card -->
 		</div> <!-- end of div#contribute-card -->
