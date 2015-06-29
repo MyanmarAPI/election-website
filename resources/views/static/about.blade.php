3bbcfd65fe7d2667e5bb4fc98603f0ae77f2aaa7
Nyan Lynn Htut <lynnhtut87@gmail.com>
2015-06-29 19:36:49 +0630

    Add lorem at static page

--- resources/views/static/about.blade.php
+++ resources/views/static/about.blade.php
@@ -6,7 +6,10 @@
 	
 	<div class="row app-container mg-top">
 		<div class="col s12" id="about">
-			<h2>About Us</h2>
+			<h4>About Us</h4>
+			
+			<p>Coming Soon....</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis iusto deleniti nesciunt illo dicta sit quisquam ea, veniam ratione earum neque labore voluptates ipsum voluptatibus at, vero vel nemo tempora.</p>
 		</div>
 	</div>
 @endsection
