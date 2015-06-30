fb76f9cc17934cb5772687b3441cb0c708f84904
Nyan Lynn Htut <lynnhtut87@gmail.com>
2015-06-30 17:23:09 +0630

    Remove softdelete

--- app/User.php
+++ app/User.php
@@ -5,14 +5,13 @@ namespace App;
 use App\Util\Gravatar;
 use Jenssegers\Mongodb\Model;
 use Illuminate\Auth\Authenticatable;
-use Jenssegers\Mongodb\Eloquent\SoftDeletes;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
 class User extends Model implements AuthenticatableContract, CanResetPasswordContract
 {
-    use Authenticatable, CanResetPassword, SoftDeletes;
+    use Authenticatable, CanResetPassword;
 
     /**
      * The database collection used by the model.
