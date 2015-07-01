a139c3cd792a7bdd3a369734f01112959828a587
Nyan Lynn Htut <lynnhtut87@gmail.com>
2015-07-01 19:18:31 +0630

    Update config for mail

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => env('MAIL_ADDRESS', 'info@maepaysoh.org'), 'name' => env('MAIL_NAME', 'Maepaysoh')],
 
     /*
     |--------------------------------------------------------------------------
