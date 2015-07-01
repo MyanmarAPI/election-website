350f5cc70804171c82bf58d6e1d6f24c5e1eef04
Nyan Lynn Htut <lynnhtut87@gmail.com>
2015-07-01 19:48:36 +0630

    Add profile edit and password change link

--- resources/views/partials/admin-toolbar.blade.php
+++ resources/views/partials/admin-toolbar.blade.php
@@ -10,6 +10,12 @@
 			<a class="waves-effect waves-teal btn-flat white-text" href="{{ url('members') }}">
 				<i class="material-icons left">supervisor_account</i>Admin Members
 			</a>
+			<a class="waves-effect waves-teal btn-flat white-text" href="{{ route('profile') }}">
+				<i class="material-icons left">settings</i>Edit Profile
+			</a>
+			<a class="waves-effect waves-teal btn-flat white-text" href="{{ route('profile.password') }}">
+				<i class="material-icons left">lock</i>Edit Password
+			</a>
 		</div>
 	</div> <!-- end of div.app-container -->
 </div> <!-- end of div#toolbar -->
