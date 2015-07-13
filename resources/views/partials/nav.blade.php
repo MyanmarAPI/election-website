17979e4991add396f4184d9f8f396ad7059abda0
Nyan Lynn Htut <lynnhtut87@gmail.com>
2015-07-13 16:27:25 +0630

    Change navigation

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,12 +1,12 @@
 @if ( Auth::check())
 <li><a href="{{ url('dashboard') }}">Dashboard</a></li>
-@endif
+<li><a href="{{ route('profile') }}">Edit Profile</a></li>
+<li><a href="{{ route('profile.password') }}">Edit Password</a></li>
+<li><a href="{{ url('auth/logout') }}">Logout</a></li>
+@else
 <li><a href="{{ url('/') }}">Home</a></li>
 <li><a href="#">Showcase</a></li>
 <li><a href="{{ url('about') }}">About</a></li>
 <li><a href="{{ url('contact') }}">Contact</a></li>
-@if ( Auth::check())
-<li><a href="{{ url('auth/logout') }}">Logout</a></li>
-@else
 <li><a href="{{ url('auth/login') }}">Login</a></li>
 @endif
