98f16b61621672e61a3b192d8ba8f495d105f62f
Nyan Lynn Htut <lynnhtut87@gmail.com>
2015-07-14 18:40:42 +0630

    Add showcase application submit data in contact

--- resources/views/static/contact.blade.php
+++ resources/views/static/contact.blade.php
@@ -10,6 +10,22 @@
 
 			<p>Coming Soon....</p>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis iusto deleniti nesciunt illo dicta sit quisquam ea, veniam ratione earum neque labore voluptates ipsum voluptatibus at, vero vel nemo tempora.</p>
+
+			<h4 id="submit-your-app">Submit Your Application</h4>
+			<p>You can also submit your application information created using Myanmar Election API. We will publicize and promote each application that uses Myanmar Election API . </p>
+			<p>The trick is as follows :</p>
+			<ul class="bullet-list">
+				<li>Applications must be made using data from the API Election</li>
+				<li>
+					Include the following items
+					<ul class="bullet-list-disc">
+						<li>Application Icon</li>
+						<li>Application Screenshoot</li>
+						<li>Description of the application</li>
+						<li>URL where the application published applications ( example : Play Store URL )</li>
+					</ul>
+				</li>
+			</ul>
 		</div>
 	</div>
 @endsection
