db7db9e441cc91fb1775540f9a389ccd0101c412
Nyan Lynn Htut <lynnhtut87@gmail.com>
2015-07-15 17:57:45 +0630

    Update endpoint lists helper

--- app/helpers.php
+++ app/helpers.php
@@ -1,11 +1,23 @@
 <?php
 
 /**
- * Helper functions lists for Election API Website
+ * Helper functions lists for Election API Website.
+ * 
+ * @package Election API Website
+ * @author Nyan Lynn Htut <naynlynnhtut@hexcores.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 if ( ! function_exists('app_type'))
-{
+{	
+	/**
+	 * Get appication type's showable name based on key.
+	 *
+	 * @param  string $type
+	 * @return string
+	 */
 	function app_type($type)
 	{
 		switch ($type) {
@@ -35,8 +47,25 @@ if ( ! function_exists('app_type'))
 
 if ( ! function_exists('endpoint_lists'))
 {
+	/**
+	 * Get endpoint lists from API Router.
+	 *
+	 * @return array
+	 */
 	function endpoint_lists()
 	{
-		return ['Sample Endpoint', 'Test Endpoint', 'Another Endpoint'];
+		try {
+			
+			return Cache::remember('endpoint_lists', 60, function() {
+				$client = new GuzzleHttp\Client();
+
+				$res = $client->get('http://api.maepaysoh.org/endpoints');
+
+				return json_decode($res->getBody(), true);	
+			});
+			
+		} catch (Exception $e) {
+			return [];
+		}
 	}
 }
