9c06a198d57d53df7ea3db03f8cb3f80ca09cc6a
Yohan Totting <tyohan@gmail.com>
2015-07-15 19:26:05 +0630

    fix copy  about page

--- resources/views/static/about.blade.php
+++ resources/views/static/about.blade.php
@@ -9,7 +9,7 @@
 			<h4>About Us</h4>
 			
 			<p>Mae Pay Soh is a open data movement for Myanmar election. The goal is to make voters easy to get information that related to Myanmar election like party, candidate, geolocation, etc.</p>
-			<p>We are partnership with UEC Myanmar, local community like Community Oriented Myanmar(COM), Google Developer Group (GDG) Yangon, Myanmar Geek Girl, and Phan Dee Yar to make this thing happen. And we invite all of you to join our movement and create a better election.</p>
+			<p>We are partnership with Union Election Commission (UEC) Myanmar, local community like Community Oriented Myanmar(COM), Google Developer Group (GDG) Yangon, Myanmar Geek Girl, and Phan Dee Yar to make this thing happen. And we invite all of you to join our movement and create a better election.</p>
 			<p>We provide all election data through Application Programming Interface(API) where developers, designer, or all creative talent could access and use for free. All data, technology and source code that we use here is free and open source. You can check it out on our <a href="https://github.com/MyanmarAPI">Github</a>.</p>
 		</div>
 	</div>
