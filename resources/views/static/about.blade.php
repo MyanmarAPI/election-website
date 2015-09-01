d0bf3bd55aeb808eb61b0312f6ca98e2adfc3d4a
Nyan Lynn Htut <lynnhtut87@gmail.com>
2015-09-01 12:16:39 +0630

    Use target blank for external links

--- resources/views/static/about.blade.php
+++ resources/views/static/about.blade.php
@@ -7,15 +7,16 @@
 	<div class="row app-container mg-top">
 		<div class="col s12" id="about">
 			<h4>About Us</h4>
+			
 			<p>MaePaySoh (“Let’s Vote!”) is an initiative that taps the talent of local Myanmar developers to help voters access essential information for Myanmar’s upcoming general elections.</p>
  
 			<p>Myanmar’s general elections will be held on November 8, 2015. What do voters need to know about electoral processes, political parties, and candidates?</p>
  
-			<p>Working together with the <a href="http://uecmyanmar.org">Union Election Commission (UEC) of Myanmar</a>, and local civil society organizations <a href="http://commyanmar.org/">Charity Oriented Myanmar (COM)</a> and the <a href="http://www.omimyanmar.org/">Open Myanmar Initiative (OMI)</a>, we are compiling a public database of information about the 6000+ candidates for national and state/region parliamentary seats, the 93 political parties contesting the elections, and answers to voters’ frequently asked questions about the functions of government institutions, electoral processes, and how to vote.</p>
+			<p>Working together with the <a href="http://uecmyanmar.org" target="_blank">Union Election Commission (UEC) of Myanmar</a>, and local civil society organizations <a href="http://commyanmar.org/" target="_blank">Charity Oriented Myanmar (COM)</a> and the <a href="http://www.omimyanmar.org/" target="_blank">Open Myanmar Initiative (OMI)</a>, we are compiling a public database of information about the 6000+ candidates for national and state/region parliamentary seats, the 93 political parties contesting the elections, and answers to voters’ frequently asked questions about the functions of government institutions, electoral processes, and how to vote.</p>
  
 			<p>The database will be available to the public through the MaePaySoh Application Programming Interface (API).</p>
  
-			<p>In partnership with <a href="http://phandeeyar.org/">Phandeeyar</a>, and the support of Myanmar developer community organizations such as <a href="https://developers.google.com/groups/chapter/105531398471796409992/">Google Developer Group (GDG) Yangon</a> and <a href="http://www.geekgirlsmyanmar.com/">Myanmar Geek Girls</a>, we’ll be holding a Hack Challenge competition that encourages Myanmar developers to unleash their talent and creativity to build applications powered by the MaePaySoh API that help Myanmar’s voters participate in the elections.</p>
+			<p>In partnership with <a href="http://phandeeyar.org/" target="_blank">Phandeeyar</a>, and the support of Myanmar developer community organizations such as <a href="https://developers.google.com/groups/chapter/105531398471796409992/" target="_blank">Google Developer Group (GDG) Yangon</a> and <a href="http://www.geekgirlsmyanmar.com/" target="_blank">Myanmar Geek Girls</a>, we’ll be holding a Hack Challenge competition that encourages Myanmar developers to unleash their talent and creativity to build applications powered by the MaePaySoh API that help Myanmar’s voters participate in the elections.</p>
  
 			<p>All data, technology, and source code we use here is free and open source. Check it out on our <a href="https://github.com/MyanmarAPI">Github</a> and <a href="http://myanmarapi.github.io/">start building your app</a>.</p>
 
