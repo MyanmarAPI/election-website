a5138482b1c7bd8ce1acf0e2ff57099a03d6c05d
Nyan Lynn Htut <lynnhtut87@gmail.com>
2015-09-07 17:04:23 +0630

    Fix exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -41,7 +41,7 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-        if (app()->environment() !== 'production') {
+        if (app()->environment() == 'production') {
             return $this->renderForProduction($request, $e);
         }
 
