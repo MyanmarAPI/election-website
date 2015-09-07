40d3f5b26d3375b8137d91a0b495d3acaf5fc6c3
Yohan Totting <tyohan@gmail.com>
2015-09-07 14:21:59 +0800

    update about page and hack challenge image

--- resources/views/static/about.blade.php
+++ resources/views/static/about.blade.php
@@ -12,17 +12,17 @@
  
 			<p>Myanmar’s general elections will be held on November 8, 2015. What do voters need to know about electoral processes, political parties, and candidates?</p>
  
-			<p>Working together with the <a href="http://uecmyanmar.org" target="_blank">Union Election Commission (UEC) of Myanmar</a>, and local civil society organizations <a href="http://commyanmar.org/" target="_blank">Charity Oriented Myanmar (COM)</a> and the <a href="http://www.omimyanmar.org/" target="_blank">Open Myanmar Initiative (OMI)</a>, we are compiling a public database of information about the 6000+ candidates for national and state/region parliamentary seats, the 93 political parties contesting the elections, and answers to voters’ frequently asked questions about the functions of government institutions, electoral processes, and how to vote.</p>
- 
+ 			<p>Working with the <a href="http://uecmyanmar.org" target="_blank">Union Election Commission (UEC)</a>, we are compiling a public database of information about the 6000+ candidates for national and state/region parliamentary seats, the 93 political parties contesting the elections, and answers to voters’ frequently asked questions about the functions of government institutions, electoral processes, and how to vote.</p>
+			
 			<p>The database will be available to the public through the MaePaySoh Application Programming Interface (API).</p>
  
-			<p>In partnership with <a href="http://phandeeyar.org/" target="_blank">Phandeeyar</a>, and the support of Myanmar developer community organizations such as <a href="https://developers.google.com/groups/chapter/105531398471796409992/" target="_blank">Google Developer Group (GDG) Yangon</a> and <a href="http://www.geekgirlsmyanmar.com/" target="_blank">Myanmar Geek Girls</a>, we’ll be holding a Hack Challenge competition that encourages Myanmar developers to unleash their talent and creativity to build applications powered by the MaePaySoh API that help Myanmar’s voters participate in the elections.</p>
+			<p>We’re holding <a href="/hackchallenge">a Hack Challenge competition</a> that encourages Myanmar developers to unleash their talent and creativity to build applications powered by the MaePaySoh API that help Myanmar’s voters participate in the elections.</p>
  
 			<p>All data, technology, and source code we use here is free and open source. Check it out on our <a href="https://github.com/MyanmarAPI">Github</a> and <a href="http://myanmarapi.github.io/">start building your app</a>.</p>
 
 			<p>The MaePaySoh team includes <a href="http://asiafoundation.org/" target="_blank">The Asia Foundation</a>, <a href="http://phandeeyar.org/" target="_blank">Phandeeyar Innovation Lab</a>, <a href="http://commyanmar.org/" target="_blank">Charity Oriented Myanmar (COM)</a>, <a href="http://www.omimyanmar.org/" target="_blank">Open Myanmar Initiative (OMI)</a>, <a href="https://developers.google.com/groups/chapter/105531398471796409992/" target="_blank">Google Developer Group (GDG) Yangon</a>, and <a href="http://www.geekgirlsmyanmar.com/" target="_blank">Myanmar Geek Girls</a>. Additional thanks to the <a href="http://www.ifes.org/myanmar" target="_blank">International Foundation for Electoral Systems (IFES)</a>, <a href="http://facebook.com" target="_blank">Facebook</a>, Ye Lin Aung, Thuya Myo Nyunt, and Nyan Lynn Htut.</p>
 
-			<p>MaePaySoh is funded by <a href="http://www.ausaid.gov.au/" target="_blank">Australian Aid</a> and <a href="http://asiafoundation.org/" target="_blank">The Asia Foundation</a>.</p>
+			<p>MaePaySoh is generously funded by <a href="http://www.ausaid.gov.au/" target="_blank">Australian Aid</a> and <a href="http://asiafoundation.org/" target="_blank">The Asia Foundation</a>.</p>
 
 
 		</div>
